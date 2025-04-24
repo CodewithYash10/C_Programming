99e316dd0f156ebf45ea417c338e7f52bbf9e3b5
Yash Dubey <yash22792@gmail.com>
2025-04-24 23:57:50 +0530

    Parcentage

---
+++ C_Programming Basic To Advance/01/variable/CW_AreaOfaCircle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+
+    int r = 5;
+    float area = 3.14 * r * r;
+
+    printf("Area of Circle is: %f",area);
+
+
+}
---
+++ C_Programming Basic To Advance/01/variable/VolumeOfaSphare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+
+    int r = 5;
+    // cin >> r;
+    float v = 4 * 3.14 * r * r * r / 3;
+
+    printf("The Volume Of Sphare is: %f",v );
+
+}
