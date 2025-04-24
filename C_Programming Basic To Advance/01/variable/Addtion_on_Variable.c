17e506b5695af159d0a8c429ca72acb50f8b7710
Yash Dubey <yash22792@gmail.com>
2025-04-24 19:41:18 +0530

    Variables

---
+++ C_Programming Basic To Advance/01/variable/Addtion_on_Variable.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+
+    int x = 5;
+    printf("%d",x);
+
+    x = x + 5;
+    printf("\n%d",x);
+
+
+    // Substration in C
+
+
+    x = x - 3;
+    printf("\n%d",x);
+
+}
