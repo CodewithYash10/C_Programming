21b653ce5ffb06c00591490e794ecbdee39fc5cf
Yash Dubey <yash22792@gmail.com>
2025-04-27 00:19:16 +0530

    New Files

--- C_Programming Basic To Advance/IF_ELSE/EvenOdd.c
+++ C_Programming Basic To Advance/IF_ELSE/EvenOdd.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int n;
     printf("Enter the Value of x: ");
-    scanf("%d",&n);
-    if (n%2==0)
+    scanf("%d", &n);
+    if (n % 2 == 0)
     {
         printf("It is Even Number");
     }
-    else{
+    else
+    {
         printf("It is odd number");
     }
-    
 
     return 0;
 }
