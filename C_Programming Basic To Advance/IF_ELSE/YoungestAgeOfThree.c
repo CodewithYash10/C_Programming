c56e44df24dc21356365e74d9e59461ffc75c5f8
Yash Dubey <yash22792@gmail.com>
2025-04-27 23:52:15 +0530

    HW DONE

--- C_Programming Basic To Advance/IF_ELSE/YoungestAgeOfThree.c
+++ C_Programming Basic To Advance/IF_ELSE/YoungestAgeOfThree.c
@@ -1,25 +1,27 @@
-#include<stdio.h>
-int main()
-{
-    int a,b,c;
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
     printf("Enter the Ram Age : ");
-    scanf("%d",&a);
-    printf("Enter the Shayam Age : ");
-    scanf("%d",&b);
+    scanf("%d", &a);
+    printf("Enter the Shyam Age : ");
+    scanf("%d", &b);
     printf("Enter the Ajay Age : ");
-    scanf("%d",&c);
-    // if(a>b){
-    //     if(a>c){
-    //         printf("Ajay is Younger");
-    //     }else{
-    //         printf("Ram is Younger");
-    //     }
-    // }else{
-    //     if(b>c){
-    //         printf("Ajay is Younger");
-    //     }else{
-    //         printf("Shyam is Younger");
-    //     }
-    // }
+    scanf("%d", &c);
+
+    if (a < b) {
+        if (a < c) {
+            printf("Ram is Younger\n");
+        } else {
+            printf("Ajay is Younger\n");
+        }
+    } else {
+        if (b < c) {
+            printf("Shyam is Younger\n");
+        } else {
+            printf("Ajay is Younger\n");
+        }
+    }
+
     return 0;
 }
