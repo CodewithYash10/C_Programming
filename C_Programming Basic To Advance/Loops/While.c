81d961a72ccb8e23c2470673bc5bfe9a56d9193e
Yash Dubey <yash22792@gmail.com>
2025-04-30 23:36:58 +0530

    While Loop Predict the output 05 done

---
+++ C_Programming Basic To Advance/Loops/While.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    // Print numbers from 1 to 100 using while loop
+    int i = 1; // Initialize i to 1
+    while(i <= 100){
+        printf("%d ", i);
+        i++;
+    }
+    return 0; // Return 0 to indicate successful completion
+}
